7433dd8516b76c2c14c09d60ac4ab136de78b3e1
kelvinchinedu1220 <pilopilosimon1990@gmail.com>
2025-03-31 22:35:12 +0100

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -135,17 +135,29 @@ VENDOR_SECURITY_PATCH := 2099-12-31
 PLATFORM_VERSION := 16.1.0
 
 ## TWRP-Specific configuration
-
 TW_THEME := portrait_hdpi
-TW_DEVICE_VERSION := TecnoCamon17Pro
+RECOVERY_SDCARD_ON_DATA := true
+TARGET_USES_MKE2FS := true
+TARGET_USE_CUSTOM_LUN_FILE_PATH := /config/usb_gadget/g1/functions/mass_storage.usb0/lun.%d/file
+TW_BRIGHTNESS_PATH := "/sys/class/leds/lcd-backlight/brightness"
+TW_DEFAULT_BRIGHTNESS := 1200
+TW_MAX_BRIGHTNESS := 4047
+TW_Y_OFFSET := 40
+TW_H_OFFSET := -50
+TW_EXCLUDE_DEFAULT_USB_INIT := true
+TW_EXCLUDE_NANO := true
+TW_EXCLUDE_APEX := true
+TW_EXCLUDE_LPDUMP := true
+TW_EXCLUDE_EXTRA_LANGUAGE := true
+TW_INCLUDE_LPTOOLS := true
+TW_EXCLUDE_BASH := true
+TW_EXCLUDE_TWRPAPP := false
 TW_EXTRA_LANGUAGES := false
 TW_INCLUDE_NTFS_3G := true
+TW_NO_SCREEN_BLANK := true
+TW_SCREEN_BLANK_ON_BOOT := true
+TW_INCLUDE_RESETPROP := true
 TW_HAS_MTP := true
-TW_EXCLUDE_TWRPAPP := true
-TW_INCLUDE_REPACKTOOLS := true
-TWRP_INCLUDE_LOGCAT := true
-TARGET_USES_LOGD := true
-TARGET_USES_MKE2FS := true
 
 # Device config
 TW_BRIGHTNESS_PATH := "/sys/class/leds/lcd-backlight/brightness"
@@ -168,3 +180,4 @@ TW_RECOVERY_ADDITIONAL_RELINK_LIBRARY_FILES += \
     $(TARGET_OUT_SHARED_LIBRARIES)/libkeymaster4.so \
     $(TARGET_OUT_SHARED_LIBRARIES)/libpuresoftkeymasterdevice.so
 TW_INCLUDE_FBE_METADATA_DECRYPT := true
+TW_USE_FSCRYPT_POLICY := 2
